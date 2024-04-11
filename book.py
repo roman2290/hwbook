851282cbcd7ab6ee44bb3ef78dfad145fa5e25c6
Roman <Roman.22.04@yandex.ru>
2024-04-11 16:44:40 +0700

    Initial commit

--- book.py
+++ book.py
@@ -1,19 +1,51 @@
-#with open('book.txt', encoding='utf-8') as f:
-    # lines = f.readline()
-    
-    # cook_book= {}
+import pprint
+with open('book.txt', encoding='utf-8') as file:
+    cook_book = {}
+    for i in file:
+        recepie_name = i.strip()
+        ingredients_count = file.readline()
+        ingredients = []
+        for p in range(int(ingredients_count)):
+            recepie = file.readline().strip().split(' | ')
+            ingredient, quantity, measure = recepie
+            ingredients.append({'ingredient_name': ingredient, 'quantitys': quantity, 'measures': measure})
+        file.readline()
+        cook_book[recepie_name] = ingredients
+pprint.pprint(cook_book)
 
+def get_shop_list_by_dishes(person_count, *dishes):
+    for dish in cook_book:
+        if dish == dishes:
+            quantity = quantity * person_count
+            print (dishes)
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+
+d ={}
+name = '2.txt'
+with open(name, 'r', encoding='utf-8') as f:
+    d[name] = [x for x in f.read().splitlines() if x]
+with open('result.txt', 'w', encoding='utf-8') as file: 
+    for key, value in sorted(d.items(), key=lambda x: len([x[1]])):  
+        file.write(key + '\n')
+        file.write(str(len(value))+ '\n')
+        file.write('\n' .join(value))
+        file.write('\n')
+        
+d = {}
+name = '1.txt'
+with open(name, 'r', encoding='utf-8') as f:
+    d[name] = [x for x in f.read().splitlines() if x]
+with open('result.txt', 'a', encoding='utf-8') as file:
+    for key, value in sorted(d.items(), key=lambda x: len([[1]])):
+        line_new = list()
+        count = 0 
+        for new_list in range(1, 3):
+            i = d[name] 
+            line_new.append(new_list)
+        for line in line_new:
+            if line:
+                count += 1       
+        file.write(key + '\n')
+        file.write(str(count) + '\n')
+        file.write('\n'.join(value[:2]))
     
-# 
-def my_cook_book():
-    with open('cook.txt', encoding='utf-8') as file:
-        file = file.readline()
-        cook_book = {}
-        for line in file.read().split('\n\n'):
-            name, _, *args = line.split('\n')
-            cook_li = []
-        for arg in args:
-            ingredient_name, quantity, measure = map(lambda x: int(x) if x.isdigit() else x, arg.split(' | '))
-            cook_li.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
-            cook_book[name] = cook_li
-        print (cook_book)
